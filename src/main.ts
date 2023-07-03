f9eb11d8b42721c8e78338739cd67da5ab7e45aa
khubaib-dev <137499579+khubaib-dev@users.noreply.github.com>
2023-07-03 19:20:43 +0500

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 import 'reflect-metadata';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(express()));
+
+  app.enableCors();
+  
   await app.listen(3000);
 }
 bootstrap();
